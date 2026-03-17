892c6c11646839e727cf1491e32dd63beed8c914
saivamshi112k5 <saivamshi2k5@gmail.com>
2026-03-17 06:53:44 +0530

    completed skills and footer section

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const menuIcon = document.getElementById("menu-icon");
 const navLinks = document.querySelector(".navlinks");
+const navItems = document.querySelectorAll(".navlinks li a");
 
 menuIcon.addEventListener("click", () => {
     navLinks.classList.toggle("active");
+});
+
+navItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        if (window.innerWidth <= 1280) {
+            navLinks.classList.remove("active");
+        }
+    });
 });
 
 const typed = new Typed('.multiple-text', {
